b631348cd66a48a68c73f03b099044ec621f4e27
Mathias <mathischerer@gmail.com>
2025-06-23 10:43:17 -0300

    estrutura de pastas e várzea

--- Site Tutorial/Introduction/1_versionCheck.py
+++ Site Tutorial/Introduction/1_versionCheck.py
@@ -1,7 +1,8 @@
-import PySide6.QtCore
+import PySide6
+from PySide6.QtCore import qVersion
 
 # Versão do PySide6
 print(PySide6.__version__)
 
 # Versão do Qt usada para compilar o PySide6
-print(PySide6.QtCore.__version__)
+print(qVersion())
